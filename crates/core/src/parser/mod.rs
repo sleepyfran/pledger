ebca96e226e571f90c3c84f2fa4eb09921f53cca
sleepyfran <fgzv@outlook.com>
2021-10-05 23:53:30 +0100

    :fire: Remove unused code & use `multispace0`

--- crates/core/src/parser/mod.rs
+++ crates/core/src/parser/mod.rs
@@ -1,25 +1,15 @@
-use nom::bytes::streaming::take_while;
+use nom::character::complete::multispace0;
 use nom::combinator::opt;
 use nom::error::Error;
 use nom::sequence::delimited;
-use nom::{Finish, IResult};
+use nom::Finish;
 
 mod ast;
 mod journal_year;
 
-pub enum ParseError {
-    InvalidYear(String),
-}
-
 /// Attempts to parse a journal from the given content, returning a result specifying
 pub fn parse_journal(content: &str) -> Result<ast::Journal, Error<&str>> {
-    delimited(spaces, journal_year::parse, opt(spaces))(content)
+    delimited(multispace0, journal_year::parse, opt(multispace0))(content)
         .finish()
         .map(|(_, year)| ast::Journal { year: Some(year) })
-}
-
-/// Parser that consumes all spaces, new lines and return characters.
-fn spaces(input: &str) -> IResult<&str, &str> {
-    let space_chars = " \t\n\r";
-    take_while(move |c| space_chars.contains(c))(input)
 }
